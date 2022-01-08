0b7e834abf7d7ea504be8ad5c1a98fc7f4298332
tUsharDeshal <97304851+tUsharDeshal@users.noreply.github.com>
2022-01-08 19:21:38 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3d5b59e3-8571-4032-a337-a1f093d48caf';
+export const MODAL_CLOSE_SEC = 2.5;
